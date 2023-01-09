633266696e5187afd889aee70c8062ce5e66c45e
Lucas Kajander <lucas.kajander@student.careeria.fi>
2023-01-09 12:54:59 +0200

    Harjoitus1

---
+++ Olio-ohjelmointi/Harjoitus1/Ajoneuvo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus1
+{
+    internal class Ajoneuvo
+    {
+        public string Merkki;
+        public int Nopeus;
+        public int Renkaat;
+
+    public Ajoneuvo(string merkki, int nopeus, int renkaat)
+    {
+            Merkki = merkki;
+            Nopeus = nopeus;
+            Renkaat = renkaat;
+
+    }
+
+    public void TulostaData()
+    {
+            Console.WriteLine("Merkki on " + Merkki);
+            Console.WriteLine("Nopeus on " + Nopeus + " kilometriä tunnissa");
+            Console.WriteLine("Renkaita on " + Renkaat);
+        }
+    }
+}
---
+++ Olio-ohjelmointi/Harjoitus1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo ajoneuvo1 = new Ajoneuvo("Bugatus", 480, 4);
+
+            ajoneuvo1.TulostaData();
+        }
+    }
+}
