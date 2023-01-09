55ae44210849ab5125b3d70fc7bb40e92851f6a9
Lucas Kajander <lucas.kajander@student.careeria.fi>
2023-01-09 13:31:10 +0200

    Harjoitus2

---
+++ Olio-ohjelmointi/Harjoitus2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            opiskelija opiskelija1 = new opiskelija("Lucas", "ABC69", 10000000);
+            opiskelija opiskelija2 = new opiskelija("Niko", "Bals420", 500000000);
+            opiskelija opiskelija3 = new opiskelija("Waltteri", "Sus21", 75000000);
+
+            opiskelija1.Tulostatiedot();
+            opiskelija2.Tulostatiedot();
+            opiskelija3.Tulostatiedot();
+
+        }
+    }
+}
---
+++ Olio-ohjelmointi/Harjoitus2/opiskelija.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+{
+    internal class opiskelija
+    {
+        public string Nimi;
+        public string Id;
+        public int Opintopisteet;
+        public opiskelija(string nimi, string id, int opintopisteet)
+        {
+            Nimi = nimi;
+            Id = id;
+            Opintopisteet = opintopisteet;
+        }
+        public void Tulostatiedot()
+        {
+            Console.WriteLine("Nimi on " + Nimi);
+            Console.WriteLine("Id on " + Id);
+            Console.WriteLine("Opintopisteitä on " + Opintopisteet);
+            Console.WriteLine();
+        }
+    }
+}
