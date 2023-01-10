76037d8b4d224530e3038c8c276dd8ba9d50abca
Lucas Kajander <lucas.kajander@student.careeria.fi>
2023-01-10 11:41:35 +0200

    Harjoitus5

---
+++ Olio-ohjelmointi/Harjoitus5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Laskin.erotus(14, 15));
+            Console.WriteLine(Laskin.summa(14, 15));
+            Console.WriteLine(Laskin.kertolasku(14, 15));
+            Console.WriteLine(Laskin.jakolasku(14, 15));
+        }
+    }
+}
